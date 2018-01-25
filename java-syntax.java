4f20d5e058eeefb5399dfcbb88f734f9ffd8d55d
Zilvinus-Peciulis <zilvinus.peciulis@gmail.com>
2018-01-25 18:00:35 +0200

    new

---
+++ java-syntax.java
@@ -0,0 +1,25 @@
+------String 
+String name[] = {"Name Named","YourName YouNamed"};
+    jList1.setListData(name);
+
+-////Basic Syntax
+..String..
+
+String[] names = new String[4];
+String names[]={"Robert","Josh"}
+  --or
+int sizeUsers = 4;
+String[] names = new String[sizeUsers];  
+  --or
+String[] names = new String[4];
+String names[0]="Robert";
+
+
+-----Vetor
+import java.util.Vector;
+private  Vector<String> namesArray = new Vector<String>();
+vectorArray.add("Robert");
+-----ArrayList
+ import java.util.ArrayList;
+ private ArrayList<String> namesArray = new ArrayList<String>();
+ namesArray.add("Robert");
