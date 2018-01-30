d7e1a2d8599c67aae483a792b345749c46010296
Zilvinus-Peciulis <zilvinus.peciulis@gmail.com>
2018-01-30 04:24:48 +0200

    new

--- java-syntax.java
+++ java-syntax.java
@@ -1,20 +1,31 @@
-------String 
-String name[] = {"Name Named","YourName YouNamed"};
-    jList1.setListData(name);
+
+
 
 -////Basic Syntax
 ..String..
+  Declaration..
 
-String[] names = new String[4];
-String names[]={"Robert","Josh"}
-  --or
-int sizeUsers = 4;
-String[] names = new String[sizeUsers];  
-  --or
-String[] names = new String[4];
-String names[0]="Robert";
+		String[] names = new String[4];
+		String names[]={"Robert","Josh"}
+		  --or
+		int sizeUsers = 4;
+		String[] names = new String[sizeUsers];  
+		  --or
+		String[] names = new String[4];
+		String names[0]="Robert";
 
+   String.methods..
+	    
+	    //Find mathed part of text 
+	       String s = "RobertKarmenJoshKaren";
+           System.out.println(s.indexOf("Kar"));//  'Kar' start after 7 letter
+           System.out.println(s.indexOf("Kar",7)); // Ignore 'Kar' after 7 letter and return 16 founded 'Kar' position
+        // Concanate Text
+           String s = "fish";
+           String s2= "tank";
+           System.out.println(s.concat(s2)); // return 'fishtank'
 
+..........................................................
 -----Vetor
 import java.util.Vector;
 private  Vector<String> namesArray = new Vector<String>();
@@ -29,7 +40,7 @@ vectorArray.add("Robert");
  ----//For loop
 
  //loop each values
- String names[] = {"Robert","Josh","Karen"};
+ String names[] = {"Robert","Karmen","Josh","Karen"};
 
  	for (String name: names){
  			if(name.startsWith("Ka"))
@@ -38,3 +49,29 @@ vectorArray.add("Robert");
  	}
 
 //or 
+
+
+
+
+
+
+
+ 	//// Random Code
+
+ 	--//Java ArrayList of ArrayList, Inseption
+
+ 	public static void main(String[] args) {
+    ArrayList<ArrayList<Integer>> outer = new ArrayList<ArrayList<Integer>>();
+    ArrayList<Integer> inner = new ArrayList<Integer>();        
+
+    inner.add(100);     
+    inner.add(200);
+    outer.add(inner); // add first list
+    inner = new ArrayList<Integer>(inner); // create a new inner list that has the same content as  
+                                           // the original inner list
+    outer.add(inner); // add second list
+
+    outer.get(0).add(300); // changes only the first inner list
+
+    System.out.println(outer);
+}
